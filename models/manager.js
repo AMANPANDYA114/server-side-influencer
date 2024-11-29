b7d5edcccc4634ab41c4c13d9295617493d9ec2f
Tonnyvellor <tony@gmail.com>
2024-11-28 23:39:15 -0800

    resolev all auths

--- models/manager.js
+++ models/manager.js
@@ -1,45 +1,40 @@
 const mongoose = require("mongoose");
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-const sitemanagerSchema = new mongoose.Schema(
-  {
-    name: {
+const sitemanagerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  resetToken: String,
+  expireToken: Date,
+  valid: {
+    type: Number,
+    default: 0,
+  },
+  tokens: [{
+    token: {
       type: String,
-      required: true,
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    phone: {
-      type: String,
-      required: true,
-    },
-    resetToken: String,
-    expireToken: Date,
-    valid: {
-      type: Number,
-      default: 0
-    },
-    tokens: [{
+  }],
+});
 
-      token: {
-        type: String,
-        // required: true
-
-      }
-    }
-    ],
-  });
-  sitemanagerSchema.methods.generateAuthToken = async function () {
+sitemanagerSchema.methods.generateAuthToken = async function () {
   try {
     const newtoken = jwt.sign({ _id: this._id }, "mynameisFenilsavaniandthisisoursdpproject");
-    // console.log(newtoken);
     this.tokens = this.tokens.concat({ token: newtoken });
     await this.save();
     return newtoken;
@@ -47,7 +42,9 @@ const sitemanagerSchema = new mongoose.Schema(
     console.log(err);
     return err;
   }
-}
-const sitemanager = mongoose.model("sitemanager", sitemanagerSchema);
+};
+
+// Check if the model already exists in mongoose.models
+const sitemanager = mongoose.models.sitemanager || mongoose.model("sitemanager", sitemanagerSchema);
 
 module.exports = sitemanager;
